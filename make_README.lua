af69237de87c55d7f6c2d23dcadfb681d72f4154
Christopher Moore <christopher.e.moore@gmail.com>
2019-05-22 12:30:22 +0800

    more fixes

--- make_README.lua
+++ make_README.lua
@@ -11,30 +11,19 @@ Some helper worksheets for my projects.
 
 Here's the CDN URLs:
 ]]}
-local fs = table()
-local function recurse(dir)
-	for f in file[dir]() do
-		if io.isdir(dir..'/'..f) then
-			if f:sub(1,1) ~= '.' then
-				recurse(dir..'/'..f)
-			end
-		elseif f:sub(-5) == '.html' then
-			fs:insert(dir..'/'..f)
-		end
-	end
-end
-recurse '.'
-for i=1,#fs do
-	assert(fs[i]:sub(1,2) == './')
-	fs[i] = fs[i]:sub(3)
-end
-fs:sort()
-for _,f in ipairs(fs) do
+
+local fs = io.rdir('.', function(f, isdir)
+	return f ~= '.git' and (isdir or f:sub(-5) == '.html')
+end):mapi(function(f)
+	assert(f:sub(1,2) == './')
+	return f:sub(3)
+end):sort():mapi(function(f)
 	local name = f:sub(1,-6)
-	s:insert('['..name..']('..base
-		..url.escape(f)
+	s:insert('['..name..']('..base..
+		url.escape(f)
 			:gsub('%%2f','/')
 			:gsub('%%2e','.')
 		..')\n')
-end
+end)
+
 file['README.md'] = s:concat'\n'
