6be57d26263afe17aaf8c4185f2c1c80bd44743f
Christopher Moore <christopher.e.moore@gmail.com>
2024-03-07 19:52:26 +0700

    whtiespace

--- make_README.lua
+++ make_README.lua
@@ -14,8 +14,8 @@ table.wrapfor(path:rdir(function(f, isdir)
 	local dir, name = path(f):getdir()
 	return name.path ~= '.git' and (isdir or name.path:sub(-5) == '.html')
 end))
-:mapi(function(f) 
-	f = f[1].path 
+:mapi(function(f)
+	f = f[1].path
 	if f:sub(1,2) == './' then f = f:sub(3) end
 	return f
 end):sort():mapi(function(f)
