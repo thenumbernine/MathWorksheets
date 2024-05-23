658b2640c9ec1d9f6d3b4c9463fdfcc7da1b75c7
thenumbernine <christopher.e.moore@gmail.com>
2024-05-23 15:40:25 +0800

    stupid javascript. trash language.  how did it become the norm?  I have no faith in humanity.

--- template.js
+++ template.js
@@ -1,30 +1,39 @@
-// I tried AngularJS, and it did the {{ }} thing right (to a small degree)
-//  but it was missing the feature of reading <input value='...'> for initialization
-//  and it was somehow extemporaneously messing up with mathjax (when my own alternative here behaves fine)
-//	and you couldn't do arbitrary javascript expressions within the {{ ... }}
-//  and from what I've read, AngularJS v2 just makes things worse.
-// so I just wrote my own.
+/*
+I tried AngularJS, and it did the {{ }} thing right (to a small degree)
+ but it was missing the feature of reading <input value='...'> for initialization
+ and it was somehow extemporaneously messing up with mathjax (when my own alternative here behaves fine)
+ and you couldn't do arbitrary javascript expressions within the {{ ... }}
+ and from what I've read, AngularJS v2 just makes things worse.
+so I just wrote my own.
 
-class Template {
-	constructor() {
-		const thiz = this;
-		
-		this.context = {};
-		this.spans = [];
+I also tried upgrading this to ES6 class, but that automatically implies 'use strict', which disabled 'with() {}', and as far as I know, eval() has no equivalent of with(), so writing to context no longer works.
+I could get around this with just copying all keys from 'this.context[]' into local variables.
+But from there, what happens to written variables?  Do they all go in 'window' ?
+Also if I switch to ES6 / classes / `use strict`, the implicit conversion of numbers and math operators no longer works and I just get a bunch of string concatenation,
+	and any time I want to use a variable-as-number I have to wrap it in (parseFloat(x)||0)
+Javascript is so fucking retarded.
+*/
 
-		setTimeout(() => {
-			const doms = document.querySelectorAll('[templated]');
-			for (let i = 0; i < doms.length; ++i) {
-				thiz.addDOM(doms[i]);
-			}
-		
-			if (thiz.done !== undefined) {
-				thiz.done();
-			}
-		}, 0);
-	}
+const Template = function() {
+	const thiz = this;
+	
+	thiz.context = {};
+	thiz.spans = [];
 
-	addDOM(dom) {
+	setTimeout(() => {
+		const doms = document.querySelectorAll('[templated]');
+		for (let i = 0; i < doms.length; ++i) {
+			thiz.addDOM(doms[i]);
+		}
+	
+		if (thiz.done !== undefined) {
+			thiz.done();
+		}
+	}, 0);
+}
+
+Template.prototype = {
+	addDOM : function(dom) {
 		const thiz = this;
 		
 		const r = /{{([^}]*)}}/;
@@ -52,41 +61,24 @@ class Template {
 		const inputs = document.getElementsByTagName('input');
 		for (let i = 0; i < inputs.length; ++i) {
 			const input = inputs[i];	
-			const refreshInput = (input) => {
-				// TODO actually interpret the type, dont just alwyas write .value since it's always a string, because JS doesn't care what the input's type is, because JS is a language made by retards.
+			thiz.context[input.name] = input.value;
+			input.onkeyup = e => {
 				thiz.context[input.name] = input.value;
-			};
-			refreshInput(input);
-			input.addEventListener('input', e => {
-				refreshInput(input);
 				thiz.refresh(true);	//refresh everything related to it
-			});
+			};
 		}
 
 		//now refresh it all
 		thiz.refresh(true);
-	}
-	
-	refresh(dontUpdateInputs) {
-		const thiz = this;
+	},
+	refresh : function(dontUpdateInputs) {
 		for (let i = 0; i < this.spans.length; ++i) {
 			const span = this.spans[i];
 			let result;
 			let error;
-			//with (this.context) 
-			{
+			with (this.context) {
 				try {
-					// make my own "with" since we're in strict mode since classes since ES6 and all JS is a giant piece of shit
-					let code = '(() => {\n';
-					for (let k in this.context) {
-						code += 'const '+k+' = thiz.context["'+k+'"];\n';
-//code += 'console.log("got '+k+' =", '+k+');\n';
-					}
-					code += 'return '+span.expr + ';\n'
-						+ '})()';
-//console.log('code', code);					
-					result = eval(code);
-//console.log('result', result);					
+					result = eval(span.expr);
 				} catch (e) {
 					result = e.message;
 					error = true;
@@ -110,9 +102,9 @@ class Template {
 			}
 		}
 	}
-}
+};
 
 let template;
-window.addEventListener('load', () => {
+window.addEventListener('load', e => { 
 	template = new Template();
 }, false);
