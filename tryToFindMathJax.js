6d9cc7af7ab97b839db7d16d20d24969b427a838
thenumbernine <christopher.e.moore@gmail.com>
2026-07-24 13:33:33 +0700

    more

--- tryToFindMathJax.js
+++ tryToFindMathJax.js
@@ -1,20 +1,31 @@
 // mathjax config
 // https://docs.mathjax.org/en/latest/web/configuration.html
-window.MathJax = {
-	tex: {
-		inlineMath: [['$', '$'], ['\\(', '\\)']]
-	},
-	svg: {
-		fontCache: 'global'
-	}
-};
+window.MathJax = window.MathJax || {};
+window.MathJax.tex = window.MathJax.tex || {};
+window.MathJax.tex.inlineMath = [['$', '$'], ['\\(', '\\)']];
+window.MathJax.svg = window.MathJax.svg || {};
+window.MathJax.svg.fontCache = 'global';
+// MathJax can easily parse and render a 500x500 matrix of expressions,
+// but as soon as you put as many macros in it (to cut down on file size), it feels the need to buffer everything, and you now have to request allocation of the entire string's size's worth of memory up front.
+window.MathJax.tex.maxBuffer = 5 * 1024 * 1024;	// macro substitution exceeding the 5k buffer size
+// And you have to declare to it how many macros are going to be in the expression as its macro substitution upper bound.
+window.MathJax.tex.maxMacros = 1024 * 1024;		// default is 1000 or so
+// They say this is to stop it from having a recursive macro expansion run-on.
+// But honestly.  Just detect loops in the recursive replacement graph.  And just output as you read macros.
+// No need for a buffer, no need to count macros, problem solved.
+//
+// The solution turns out to be, you must define your macros in the MathJax.tex.macros config object before initializing MathJax.
+// Only then it will use O(1) memory instead of O(n) .... smfh idk why.
+// Hmm but that isn't 100% true, I still do need to raise the buffer sizes, but it does seem to be more stable with macros written in the MathJax config.
+
 
 const tryToFindMathJax = {};
 
 tryToFindMathJax.urls = [
+	// TODO just use CDN.  my site is down anyways.  i hate being dependent on the internet.  but the whole world has gone retarded.
 	'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js',
-	'file:///home/chris/Projects/christopheremoore.net/MathJax/es5/tex-svg.js',
-	'/MathJax/es5/tex-svg.js',
+	//'file:///home/chris/Projects/christopheremoore.net/MathJax/es5/tex-svg.js',
+	//'/MathJax/es5/tex-svg.js',
 ];
 
 tryToFindMathJax.loadScript = function(args) {
